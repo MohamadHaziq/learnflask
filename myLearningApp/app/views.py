93b594e5d2217857e688aaf3d2057e165f02b99e
Haziq Roslan <haziq.roslan@digitas.com>
2020-05-10 18:06:34 +0800

    session close commit

--- myLearningApp/app/views.py
+++ myLearningApp/app/views.py
@@ -84,6 +84,22 @@ class MyForm(DynamicForm):
     field2 = StringField(('Field2'),
         description=('Your field number two!'), widget=BS3TextFieldWidget())
 
+class MyFormView(SimpleFormView):
+    form = MyForm
+    form_title = 'This is my first form view'
+    message = 'My form submitted'
+
+    def form_get(self, form):
+        form.field1.data = 'This was prefilled'
+
+    def form_post(self, form):
+        # post process form
+        flash(self.message, 'info')
+
+appbuilder.add_view(MyFormView, "My form View", icon="fa-group", label=_('My form View'),
+                     category="My Forms", category_icon="fa-cogs")
+
+
 @appbuilder.app.errorhandler(404)
 def page_not_found(e):
     return (
