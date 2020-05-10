64ebc70ffcbbf144f5db0e314819d1b2c029418c
Haziq Roslan <haziq.roslan@digitas.com>
2020-05-10 16:33:43 +0800

    follow app builder tutorial

--- myLearningApp/app/views.py
+++ myLearningApp/app/views.py
@@ -1,6 +1,14 @@
-from flask import render_template
+from flask import render_template, flash
 from flask_appbuilder.models.sqla.interface import SQLAInterface
-from flask_appbuilder import ModelView, ModelRestApi
+from flask_appbuilder import ModelView, ModelRestApi, SimpleFormView
+from flask_appbuilder import AppBuilder, BaseView, expose, has_access
+from app import appbuilder
+from wtforms import Form, StringField
+from wtforms.validators import DataRequired
+from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
+from flask_appbuilder.forms import DynamicForm
+from flask import flash
+from flask_babel import lazy_gettext as _
 
 from . import appbuilder, db
 
@@ -36,6 +44,45 @@ from . import appbuilder, db
     Application wide 404 error handler
 """
 
+class MyView(BaseView):
+
+    default_view = 'method1'
+
+    @expose('/method1/')
+    @has_access
+    def method1(self):
+        # do something with param1
+        # and return to previous page or index
+        return 'Hello'
+
+    @expose('/method2/<string:param1>')
+    @has_access
+    def method2(self, param1):
+        # do something with param1
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        return param1
+    
+    @expose('/method3/<string:param1>')
+    @has_access
+    def method3(self, param1):
+        # do something with param1
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        self.update_redirect()
+        return self.render_template('method3.html',
+                            param1 = param1)
+
+appbuilder.add_view(MyView, "Method1", category='My View')
+appbuilder.add_link("Method2", href='/myview/method2/john', category='My View')
+appbuilder.add_link("Method3", href='/myview/method3/Haziq', category='My View')
+
+class MyForm(DynamicForm):
+    field1 = StringField(('Field1'),
+        description=('Your field number one!'),
+        validators = [DataRequired()], widget=BS3TextFieldWidget())
+    field2 = StringField(('Field2'),
+        description=('Your field number two!'), widget=BS3TextFieldWidget())
 
 @appbuilder.app.errorhandler(404)
 def page_not_found(e):
